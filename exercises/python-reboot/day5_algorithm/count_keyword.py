af5baada475791f4d8204e566ed5b2970f7cc57b
yuxiaoyun2 <47133205+yuxiaoyun2@users.noreply.github.com>
2026-04-24 23:16:33 +0900

    Add Day5 Python reboot exercises

---
+++ exercises/python-reboot/day5_algorithm/count_keyword.py
@@ -0,0 +1,12 @@
+#返回数量
+def count_keyword(data, key:str):
+    count = 0
+    for item in data:
+        if key.lower() in item.lower():
+            count += 1
+            
+    return count
+
+if __name__ == "__main__":
+    data = ["Apple","banana","APP"]
+    print(count_keyword(data,"app"))
