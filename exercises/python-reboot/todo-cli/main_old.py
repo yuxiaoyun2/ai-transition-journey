1b8a68be0ffd5696a73767f5f748a4f7bcee86e7
yuxiaoyun2 <47133205+yuxiaoyun2@users.noreply.github.com>
2026-05-20 22:26:31 +0900

    chore: rename project directory to todo-cli

---
+++ exercises/python-reboot/todo-cli/main_old.py
@@ -0,0 +1,38 @@
+import sys
+from todo_manager import TodoManager
+
+todo = TodoManager()
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python main.py [add/list/done/delete]")
+        return
+
+    command = sys.argv[1]
+
+    if command == "add":
+        title = sys.argv[2]
+        todo.add_task(title)
+
+    elif command == "list":
+        todo.list_tasks()
+
+    elif command == "done":
+        index = int(sys.argv[2])
+        task = todo.mark_done(index)
+        
+        if task:
+            print(task.display())
+        else:
+            print("task no exist")
+        
+
+    elif command == "delete":
+        index = int(sys.argv[2])
+        todo.delete_task(index)
+
+    else:
+        print("Unknown command")
+
+if __name__ == "__main__":
+    main()
