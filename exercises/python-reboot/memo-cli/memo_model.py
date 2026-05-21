a2bf203e947aefb49785311f3079ae772f119e26
yuxiaoyun2 <47133205+yuxiaoyun2@users.noreply.github.com>
2026-05-22 00:59:05 +0900

    Day26 strengthen Python basics and start memo project

---
+++ exercises/python-reboot/memo-cli/memo_model.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass, asdict
+from datetime import datetime
+
+@dataclass
+class memo:
+    index: int
+    title: str
+    content: str
+    created_at: str
+    updated_at: str
+    status: str = "normal"
+    
+    def to_dict(self):
+        return asdict(self)
+    
+    @classmethod
+    def create(cls, index: int, title: str, content: str):
+        now = datetime.now().isoformat(timespec="seconds")
+        return cls(
+            index = index,
+            title = title,
+            content = content,
+            created_at = now,
+            updated_at = now
+        )
